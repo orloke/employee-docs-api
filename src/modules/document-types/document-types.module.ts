786fe034c9bea369ff41be863656e09506a71a5a
Junior Dering <juniord@lepvtech.com.br>
2025-07-20 19:45:55 -0300

    feat: create document type module

--- src/modules/document-types/document-types.module.ts
+++ src/modules/document-types/document-types.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
-import { DocumentTypesService } from './document-types.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DocumentTypeRepository } from './document-type.repository';
 import { DocumentTypesController } from './document-types.controller';
+import { DocumentTypesService } from './document-types.service';
+import { DocumentType } from './entities/document-type.entity';
+import { IDocumentsTypesRepository } from './interfaces/documents-repository.interface';
+import { IDocumentTypesService } from './interfaces/documents-service.interface';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DocumentType])],
   controllers: [DocumentTypesController],
-  providers: [DocumentTypesService],
+  providers: [
+    {
+      provide: IDocumentTypesService,
+      useClass: DocumentTypesService,
+    },
+    {
+      provide: IDocumentsTypesRepository,
+      useClass: DocumentTypeRepository,
+    },
+  ],
 })
 export class DocumentTypesModule {}
